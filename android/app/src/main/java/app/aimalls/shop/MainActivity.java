b5b24055406af5965f2a410ebdf51ff6568dd81b
Gavs Matterig <hypergavs@gmail.com>
2023-06-24 13:27:51 +0800

    init

---
+++ android/app/src/main/java/app/aimalls/shop/MainActivity.java
@@ -0,0 +1,5 @@
+package app.aimalls.shop;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'aimalls-app',
+  appId: 'app.aimalls.shop',
+  appName: 'AIMalls',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
