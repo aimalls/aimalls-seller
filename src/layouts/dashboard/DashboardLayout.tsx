c085542dd6a04735d993c2648c27a195092e069e
hypergavs <hypergavs@gmail.com>
2023-07-31 02:51:14 +0800

    s

--- src/layouts/dashboard/DashboardLayout.tsx
+++ src/layouts/dashboard/DashboardLayout.tsx
@@ -1,9 +1,12 @@
-import { IonButton, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from "@ionic/react";
-import { mail, menuOutline } from "ionicons/icons";
+import { IonAvatar, IonButton, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from "@ionic/react";
+import { bagHandleOutline, heartOutline, home, homeOutline, logOutOutline, mail, menuOutline, peopleOutline, personCircleOutline, receiptOutline } from "ionicons/icons";
 import { FC } from "react";
 import DashboardRoutes from "../../routes/DashboardRoutes";
 import { processLogoutToAPI } from "../../requests/auth.request";
 import { useHistory } from "react-router";
+import '../../styles/pages/dashboard/DashboardLayout.scss'
+
+import avatar from '../../assets/images/attractive-cheerful-silly-blond-asian-girl-pointing-down-index-finger-look-camera-happy-optimistic-smile-propose-good-recommendation-standing-white-wall.jpg'
 
 export interface iProps {}
 
@@ -24,32 +27,58 @@ export const DashboardLayout: FC<iProps> = (props): JSX.Element => {
     
 
     return (
-        <IonSplitPane id="dashboard-layout" when="md" contentId="dashboard-content">
-            <IonMenu contentId='dashboard-content' className='dashboard-navigation-content'>
-                <IonList>
-                    <IonItem lines="full" routerLink="/dashboard">
-                        <IonLabel slot='end' color={'primary'} className='dashboard-navigation-link'>Dashboard</IonLabel>
-                    </IonItem>
-                    <IonItem lines="full" routerLink="/dashboard/tasks">
-                        <IonLabel slot='end' color={'primary'} className='dashboard-navigation-link'>Tasks</IonLabel>
-                    </IonItem>
-                    <IonItem lines='full' onClick={() => processLogout()}>
-                        <IonLabel slot='end' color={'primary'} className='dashboard-navigation-link'>
-                            Log out
-                        </IonLabel>
-                    </IonItem>
-                    <IonButton className='ion-margin-top' fill='solid' shape='round' expand="full" style={{textTransform: "lowercase"}}>
-                        <IonIcon slot="start" icon={mail}></IonIcon>
-                        support@aimalls.app
-                    </IonButton>
-                </IonList>
-            </IonMenu>
-            <IonPage id="dashboard-content">
-                <IonRouterOutlet>
-                    <DashboardRoutes />
-                </IonRouterOutlet>
-            </IonPage>
-        </IonSplitPane>
+        <div id="dashboard-layout">
+            <IonSplitPane  when="md" contentId="dashboard-content">
+                <IonMenu contentId='dashboard-content' className='dashboard-navigation-content'>
+                    <IonList  style={{ background: 'none' }} lines='none' id="dashboard-list-menu">
+                        <IonAvatar>
+                            <img src={avatar} alt="avatar" height={80} width={80} style={{borderRadius: "15px"}} />
+                        </IonAvatar>
+                        <div style={{paddingBottom: "50px"}}>
+                            <div style={{ fontSize: "15px", color: "#fff", padding: "10px 0", fontFamily: "Poppins-Regular" }}>Hey,</div>
+                            <div style={{ fontSize: "15px", color: "#fff",fontFamily: "Poppins-Regular" }}>Totoy Bibs</div>
+                        </div>
+                        <IonItem lines="none" routerLink="/dashboard" className={ navigation.location.pathname == "/dashboard" ? "active" : ""}>
+                            <IonIcon color={'primary'} icon={homeOutline} slot="start"></IonIcon>
+                            <IonLabel color={'light'} className='dashboard-navigation-link'>Dashboard</IonLabel>
+                        </IonItem>
+                        <IonItem lines="none">
+                            <IonIcon color={'primary'} icon={personCircleOutline} slot="start"></IonIcon>
+                            <IonLabel color={'light'} className='dashboard-navigation-link'>Profile</IonLabel>
+                        </IonItem>
+                        <IonItem lines="none">
+                            <IonIcon color={'primary'} icon={bagHandleOutline} slot="start"></IonIcon>
+                            <IonLabel color={'light'} className='dashboard-navigation-link'>My Bag</IonLabel>
+                        </IonItem>
+                        <IonItem lines="none">
+                            <IonIcon color={'primary'} icon={heartOutline} slot="start"></IonIcon>
+                            <IonLabel color={'light'} className='dashboard-navigation-link'>Favorites</IonLabel>
+                        </IonItem>
+                        <IonItem lines="none">
+                            <IonIcon color={'primary'} icon={receiptOutline} slot="start"></IonIcon>
+                            <IonLabel color={'light'} className='dashboard-navigation-link'>Orders</IonLabel>
+                        </IonItem>
+                        <IonItem lines="none">
+                            <IonIcon color={'primary'} icon={peopleOutline} slot="start"></IonIcon>
+                            <IonLabel color={'light'} className='dashboard-navigation-link'>Start Selling</IonLabel>
+                        </IonItem>
+
+                        <div  style={{ borderTop: "1px solid #f3f3f3", margin: "20px 20px 40px 20px" }}></div>
+                        <IonItem lines='none' onClick={() => processLogout()}>
+                            <IonIcon color={'primary'} icon={logOutOutline} slot="start"></IonIcon>
+                            <IonLabel slot='start' color={'light'} className='dashboard-navigation-link'>
+                                Log out
+                            </IonLabel>
+                        </IonItem>
+                    </IonList>
+                </IonMenu>
+                <IonPage id="dashboard-content">
+                    <IonRouterOutlet>
+                        <DashboardRoutes />
+                    </IonRouterOutlet>
+                </IonPage>
+            </IonSplitPane>
+        </div>
     )
 };
 export default DashboardLayout;
